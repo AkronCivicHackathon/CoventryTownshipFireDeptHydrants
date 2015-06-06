ec6e51269f766911db69ea47d852b376e762ea01
gsvolt <gsvolt7@gmail.com>
2015-06-06 00:16:48 -0400

    added a script that extracts lat lng from original csv

--- scripts/csv2latlng.sh
+++ scripts/csv2latlng.sh
@@ -1,3 +1,3 @@
 DATA=../data/csv/SouthSummitHydrants.csv
-OUTPUT=../data/csv/SouthSummitHydrantsLatLng2.csv
-tail -n +1 $DATA | awk -F "," '{if(NR>1 && $6!="0"){print $6,$5;}}' > $OUTPUT
+OUTPUT=../data/csv/SouthSummitHydrantsLatLng.csv
+tail -n +1 $DATA | awk -F "," '{if(NR>1 && $6!="0"){printf "%s,%s\n",$6,$5;}}' > $OUTPUT
